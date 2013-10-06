056b30857ac071b4d7f25b017bb889200359fb12
Benjamin Zaporzan <benzaporzan@gmail.com>
2013-10-05 23:16:15 -0400

    fixed up ubuntu to make it work on linux

--- src/tests/test_kampf.cpp
+++ src/tests/test_kampf.cpp
@@ -3,14 +3,9 @@
 
 #include "KF_test.hpp"
 
-#include <SDL2\SDL.h>
-
-
 int main(int argc, char *argv[]) {
   TEST_NAME("test_kampf.cpp");
   auto kampf = Kampf(enumInitType::Server); 
-
-  
 
   return 0;
 }
