91cc7597580a349a16d7f12e595485845b346c53
Benjamin Zaporzan <benzaporzan@gmail.com>
2013-10-23 19:53:42 -0400

    more work on lua vector type

--- src/l_Vector.cpp
+++ src/l_Vector.cpp
@@ -29,11 +29,80 @@ static int l_Vector_Vector(lua_State *L) {
 }
 
 static int l_Vector_index(lua_State *L) {
+  Vector* vector = *static_cast<Vector**>
+    (luaL_checkudata(L, 1, LUA_USERDATA_VECTOR));
 
+  auto numargs = lua_gettop(L);
+  if (numargs > 2) {
+    luaL_error(L, "LUA_ERROR: Wrong number of arguments, %d provided",
+	       numargs);
+  }
+  if (lua_isnumber(L, 2)) {
+    integerType index = lua_tointeger(L, 2);
+    lua_pop(L, 1);
+    
+    if (index <= 0 || index > 3) {
+      luaL_error(L, "LUA_ERROR: Index out of range --> %d", index);
+    }
+
+    switch(index) {
+    case 1:
+      lua_pushnumber(L, vector->x);
+      break;
+    case 2:
+      lua_pushnumber(L, vector->y);
+      break;
+    case 3:
+      lua_pushnumber(L, vector->z);
+      break;
+    }
+
+    return 1;
+  }
+  else if (lua_isstring(L, 2)) {
+    stringType sIndex = lua_tostring(L, 2);
+    
+    
+
+    if (sIndex == "x") {
+      lua_pushnumber(L, vector->x);
+    }
+    else if (sIndex == "y") {
+      lua_pushnumber(L, vector->y);
+    }
+    else if (sIndex == "z") {
+
+      lua_pushnumber(L, vector->z);
+    }
+    else {
+      luaL_error(L, "LUA_ERROR: Index out of range --> %s",
+		 sIndex.c_str());
+    }
+
+    return 1;
+  }
+  else {
+    luaL_error(L, "LUA_ERROR: Incorrect type");
+    return 0;
+  }
+  return 0;
 }
 
 static int l_Vector_tostring(lua_State *L) {
+  Vector* vector = *static_cast<Vector**>
+    (luaL_checkudata(L, 1, LUA_USERDATA_VECTOR));
 
+  std::string output = "";
+  output += "V(";
+  output += std::to_string(vector->x);
+  output += ", ";
+  output += std::to_string(vector->y);
+  output += ", ";
+  output += std::to_string(vector->z);
+  output += ")";
+  lua_pushstring(L, output.c_str());
+
+  return 1;
 }
 
 static const struct luaL_Reg l_Vector_Registry [] = {
