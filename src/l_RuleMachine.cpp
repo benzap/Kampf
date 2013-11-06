78ccf8820539fa169bf06131c9fb0d0485c7b6bb
Benjamin Zaporzan <benzaporzan@gmail.com>
2013-11-06 18:29:04 -0500

    Started implementing rulemachine within kampf

---
+++ src/l_RuleMachine.cpp
@@ -0,0 +1,99 @@
+#include "l_RuleMachine.hpp"
+
+void lua_registerRuleMachine(lua_State *L,
+			     RuleMachine* rulemachine) {
+    //push our rulemachine point into the registry index table
+    lua_pushstring(L, LUA_REGISTRY_RULEMACHINE);
+    lua_pushlightuserdata(L, (void *) rulemachine);
+    lua_settable(L, LUA_REGISTRYINDEX);
+}
+
+boolType lua_hasRuleMachine(lua_State *L) {
+    lua_pushstring(L, LUA_REGISTRY_RULEMACHINE);
+    lua_gettable(L, LUA_REGISTRYINDEX);
+    if (lua_islightuserdata(L, -1)) {
+	RuleMachine* rulemachine = (RuleMachine *) lua_touserdata(L, -1);
+	if (rulemachine != nullptr) {
+	    return true;
+	}
+    }
+    return false;
+}
+
+RuleMachine* lua_getRuleMachine(lua_State *L) {
+    lua_pushstring(L, LUA_REGISTRY_RULEMACHINE);
+    lua_gettable(L, LUA_REGISTRYINDEX);
+    RuleMachine* rulemachine = (RuleMachine *) lua_touserdata(L, -1);
+    return rulemachine;
+}
+
+void lua_pushrulemachine(lua_State *L) {
+    RuleMachine* rulemachine = nullptr;
+    if (lua_hasRuleMachine(L)) {
+	rulemachine = lua_getRuleMachine(L);
+    }
+
+    RuleMachine** rulePtr = static_cast<RuleMachine**>
+	(lua_newuserdata(L, sizeof(RuleMachine*)));
+
+    *rulePtr = rulemachine;
+
+    luaL_getmetatable(L, LUA_USERDATA_RULEMACHINE);
+    lua_setmetatable(L, -2);
+}
+
+boolType lua_isrulemachine(lua_State *L, int index) {
+    if (lua_isuserdata(L, 1)) {
+	RuleMachine* rulemachine = *static_cast<RuleMachine**>
+	    (luaL_checkudata(L, index, LUA_USERDATA_RULEMACHINE)); 
+	if (rulemachine != NULL) {
+	    return true;
+	}
+	return false;
+    }
+}
+
+RuleMachine* lua_torulemachine(lua_State *L, int index) {
+    RuleMachine* rulemachine = *static_cast<RuleMachine**>
+	(luaL_checkudata(L, index, LUA_USERDATA_RULEMACHINE));
+    if (rulemachine == NULL) {
+	luaL_error(L, "Provided userdata is not of type 'Rulemachine'");
+    }
+    return rulemachine;
+}
+ 
+static int l_RuleMachine_isrulemachine(lua_State *L) {
+    if (lua_isrulemachine(L, 1)) {
+	lua_pushboolean(L, 1);
+    }
+    else {
+	lua_pushboolean(L, 0);
+    }
+    return 1;
+}
+
+static int l_RuleMachine_addRule(lua_State *L) {
+    
+}
+
+static const struct luaL_Reg l_rulemachine_kampf [] = {
+    {"isRuleMachine", l_RuleMachine_isrulemachine},
+    {NULL, NULL}
+};
+ 
+static const struct luaL_Reg l_rulemachine [] = {
+    {"addRule", l_RuleMachine_addRule},
+    {NULL, NULL}
+};
+
+int luaopen_rulemachine(lua_State *L, RuleMachine* rulemachine) {
+    //Rule Machine
+    luaL_newmetatable(L, LUA_USERDATA_RULEMACHINE);
+    lua_pushvalue(L, -1);
+    lua_setfield(L, -2, "__index");
+    luaL_register(L, NULL, l_rulemachine);
+
+    luaL_register(L, KF_LUA_LIBNAME, l_rulemachine_kampf);
+
+    return 1;
+}
---
+++ src/l_RuleMachine.hpp
@@ -0,0 +1,40 @@
+#ifndef L_RULEMACHINE__HPP
+#define L_RULEMACHINE__HPP
+//DESCRIPTION
+/*
+  Lua binding for the RuleMachine class instance
+ */
+
+//CLASSES
+
+//INCLUDES
+#include "KF_globals.hpp"
+#include "KF_utilities.hpp"
+#include "LuaScript.hpp"
+#include "kampf.hpp"
+#include "RuleMachine.hpp"
+
+//DEFINITIONS
+#define LUA_REGISTRY_RULEMACHINE "l_kf.rulemachine"
+#define LUA_USERDATA_RULEMACHINE "l_kf.rulemachine_ud"
+
+//MACROS
+
+//TYPEDEFS
+
+//FUNCTIONS
+
+void lua_registerRuleMachine(lua_State *L, RuleMachine* rulemachine);
+boolType lua_hasRuleMachine(lua_State *L);
+RuleMachine* lua_getRuleMachine(lua_State *L);
+
+void lua_pushrulemachine(lua_State *L);
+boolType lua_isrulemachine(lua_State *L, int index);
+RuleMachine* lua_torulemachine(lua_State *L, int index);
+
+int luaopen_rulemachine(lua_State *L, RuleMachine* rulemachine);
+
+//BEGIN
+
+
+#endif //END L_RULEMACHINE__HPP
