ad2f79f134c5a915cb76a79f9c35c80f191ea6f0
Benjamin Zaporzan <benzaporzan@gmail.com>
2013-11-26 23:14:17 -0500

    more work on drawable

---
+++ src/tests/test_SDLAssetManager.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cassert>
+
+#include "KF_test.hpp"
+
+Entity* create_pacman() {
+    auto entity = new Entity("pacman");
+    
+    //pacman needs a position, which can be defined with a physics
+    //component. The graphics system also tries to find a physics
+    //component in order to draw the graphics component correctly
+    auto physicsComponent = createPhysicsComponent("physics");
+    entity->addComponent(physicsComponent);
+    
+    physicsComponent->setPosition({0,0,0});
+
+    auto graphicsComponent = createGraphicsComponent("graphics");
+    entity->addComponent(graphicsComponent);
+
+    //the keyname of the drawable that this component refers to
+    graphicsComponent->setDrawableKey("pacman");
+
+    return entity;
+}
+
+int main(int argc, char *argv[]) {
+  TEST_NAME("test_SDLAssetManager.cpp");
+  auto kampf = Kampf(enumInitType::Basic); 
+  // auto lua = kampf.getLua();
+  // lua->loadScript(KF_INIT_FILE);
+    
+  auto assetManager = SDLAssetManager::getInstance();
+  assetManager->addBMP("pacman", "assets/pacman.bmp");
+
+  TEST_BOOL(assetManager->hasDrawable("pacman"), "hasDrawable true");
+  TEST_BOOL(!assetManager->hasDrawable("batmat"), "hasDrawable false");
+
+  //removing drawable
+  assetManager->removeDrawable("pacman");
+  TEST_BOOL(!assetManager->hasDrawable("pacman"), "removeDrawable");
+
+  //reading the drawable
+  assetManager->addBMP("pacman", "assets/pacman.bmp");
+
+  //creating an entity that holds pacman image/sprite
+  auto pacman = create_pacman();
+
+  auto entityManager = EntityManager::getInstance();
+  entityManager->addEntity(pacman);
+
+  kampf.runMainLoop(5000);
+  return 0;
+}
+
