428b1adb2109a736df88c975dcd659b0ff7d9e44
Benjamin Zaporzan <benzaporzan@gmail.com>
2013-12-02 23:22:58 -0500

    finished custom attribute unit tests

--- src/tests/test_customAttribute.lua
+++ src/tests/test_customAttribute.lua
@@ -12,29 +12,39 @@ test_case(
 		test_assert("getInteger", customAttr_num:getInteger() == 10);
 		test_assert("get (int)", customAttr_num:get() == 10);
 
+		customAttr_num:setInteger(13)
+		test_assert("setInteger - test", customAttr_num:get() == 13)
+
 		local customAttr_num2 = kf.CustomAttribute(3.14)
 		print(customAttr_num2)
 		test_assert("getFloat", customAttr_num2:getFloat() == 3.14);
 		test_assert("get (float)", customAttr_num2:get() == 3.14);
 
+		customAttr_num2:setFloat(2.73)
+		test_assert("setFloat -test", customAttr_num2:get() == 2.73)
+
 		local customAttr_string = kf.CustomAttribute("Hello")
 		print(customAttr_string)
-		test_assert("getString", customAttr_string:getString() == "Hello")
+		test_assert("getString",customAttr_string:getString() == "Hello")
 		test_assert("get (string)", customAttr_string:get() == "Hello")
+
+		customAttr_string:setString("World!")
+		test_assert("setString - test", customAttr_string:get() == "World!")
 
 		local customAttr_array = kf.CustomAttribute{2,2,4,4}
 		print(customAttr_array)
-
 		local array = customAttr_array:getArray()
 		test_assert("getArray",table.eq(array, {2,2,4,4}))
 		local array = customAttr_array:get()
 		test_assert("get (array)", table.eq(array, {2,2,4,4}))
 
+		customAttr_array:setArray{4,2,4,2}
+		local array = customAttr_array:get()
+		test_assert("setArray - test", table.eq(array, {4,2,4,2}))
+
 		local customAttr_char = kf.CustomAttribute()
 		customAttr_char:setChar("a")
 		print(customAttr_char)
-
-		print(customAttr_char:getChar())
 		test_assert("getChar", customAttr_char:getChar() == "a")
 		test_assert("get (char)", customAttr_char:get() == "a")
 
