9886b3b15fe3f05ede3f62d8b07d5ef6459ba07c
Benjamin Zaporzan <benzaporzan@gmail.com>
2014-01-05 15:24:46 -0500

    more work on collision

---
+++ src/collision/collision_circle.cpp
@@ -0,0 +1,3 @@
+#include "collision_circle.hpp"
+
+
---
+++ src/collision/collision_circle.hpp
@@ -0,0 +1,30 @@
+#ifndef COLLISION_CIRCLE__HPP
+#define COLLISION_CIRCLE__HPP
+//DESCRIPTION
+/*
+  This file includes functions for dealing with the generation of
+  circles bounds based on a given polygon.
+ */
+
+//CLASSES
+
+//INCLUDES
+#include "../KF_globals.hpp"
+#include "../KF_utilities.hpp"
+#include "../KF_Math.hpp"
+
+//DEFINITIONS
+
+//MACROS
+
+//TYPEDEFS
+
+//FUNCTIONS
+namespace Collision {
+
+    boolType check_circle_circle(COL_circle, COL_circle);
+}
+//BEGIN
+
+
+#endif //END COLLISION_CIRCLE__HPP
