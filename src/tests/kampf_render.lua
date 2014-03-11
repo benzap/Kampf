dac7055fb1f20ca446290711c325c5fed11ba292
Benjamin Zaporzan <btzaporz@lakeheadu.ca>
2014-03-10 21:53:04 -0400

    added more assets, beginning to do more work on collision

--- src/tests/kampf_render.lua
+++ src/tests/kampf_render.lua
@@ -21,17 +21,71 @@ function createPacman()
 	return entity
 end
 
+function createRedCircle()
+	local entity = kf.Entity("red-circle")
+
+	local physicsComponent = kf.PhysicsComponent("physical")
+	entity:addComponent(physicsComponent)
+	physicsComponent:setPosition(kf.Vector3(0, 0, 0))
+
+	local graphicsComponent = kf.GraphicsComponent("graphical")
+	entity:addComponent(graphicsComponent)
+	graphicsComponent:setDrawableKey("red-circle")
+
+	local collisionComponent = kf.CollisionComponent("collidable")
+	entity:addComponent(collisionComponent)
+	collisionComponent:setPhysicsRelation(physicsComponent)
+	collisionComponent:setType("CIRCLE")
+	collisionComponent:setOrigin(kf.Vector3(50, 50, 0))
+	collisionComponent:setRadius(50)
+	
+	return entity
+end
+
+function createBlueCircle()
+	local entity = kf.Entity("blue-circle")
+
+	local physicsComponent = kf.PhysicsComponent("physical")
+	entity:addComponent(physicsComponent)
+	physicsComponent:setPosition(kf.Vector3(0, 0, 0))
+
+	local graphicsComponent = kf.GraphicsComponent("graphical")
+	entity:addComponent(graphicsComponent)
+	graphicsComponent:setDrawableKey("blue-circle")
+
+	local collisionComponent = kf.CollisionComponent("collidable")
+	entity:addComponent(collisionComponent)
+	collisionComponent:setPhysicsRelation(physicsComponent)
+	collisionComponent:setType("CIRCLE")
+	collisionComponent:setOrigin(kf.Vector3(50, 50, 0))
+	collisionComponent:setRadius(50)
+	
+	return entity
+end
+
+
+
+
 local pacman = createPacman()
-local pacman2 = createPacman()
+local redCircle = createRedCircle()
+local blueCircle = createBlueCircle()
+blueCircle:getComponentsByFamily("PHYSICS")[1]:setPosition(kf.Vector3(100,100,0)) 
 
 local assetManager = kf.SDLAssetManager()
 
-local pacDrawable = assetManager:addBMP("pacman", "./assets/pacman.bmp")
-pacDrawable:setColorKey(255, 255, 255)
+local asset_pacDrawable = assetManager:addBMP("pacman", "./assets/pacman.bmp")
+asset_pacDrawable:setColorKey(255, 255, 255)
+
+local asset_redCircle = assetManager:addBMP("red-circle", "./assets/red_circle.bmp")
+asset_redCircle:setColorKey(255, 255, 255)
+
+local asset_blueCircle = assetManager:addBMP("blue-circle", "./assets/blue_circle.bmp")
+asset_blueCircle:setColorKey(255, 255, 255)
 
 local entityManager = kf.EntityManager()
 entityManager:addEntity(pacman)
-entityManager:addEntity(pacman2)
+entityManager:addEntity(redCircle)
+entityManager:addEntity(blueCircle)
 
 local renderWindow = kf.getRenderWindow()
 renderWindow:setWindowSize(1000, 600)
