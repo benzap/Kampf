f6cf57d8aeaff9e8137632c33432d0bd02c1cacc
Benjamin Zaporzan <benzaporzan@gmail.com>
2014-06-28 15:18:24 -0400

    Finished setting up physics test case

--- src/tests/test_particlephysics.lua
+++ src/tests/test_particlephysics.lua
@@ -48,11 +48,39 @@ if KF_OS ~= "LINUX" then
 	renderWindow:setClearColor(17,17,17)
 end
 
+local __text_increment = 0
+function createText(x,y)
+	local text = text:new{
+		name = "textlabel_" .. __text_increment,
+		font = "Inconsolata-normal-12",
+		physics = {
+			position = {x, y}
+		},
+		graphics = {
+			index = 50,
+		},
+	}
+	__text_increment = __text_increment + 1
+	return text
+end
+
+local padding = 5
+local font_size = 12
+local label_penetration = createText(padding, padding)
+label_penetration:setText("Penetration: ")
+local label_normal = createText(padding, padding + font_size)
+label_normal:setText("Contact Normal: ")
+local label_time = createText(padding, padding + font_size * 2)
+label_time:setText("Time: ")
+local label_time_collision = createText(padding, padding + font_size * 3)
+label_time_collision:setText("Time Collision: ")
+
 local redCircle = sprite:new{
 	name = "red-circle",
 	physics = {
 		position = {200, 200, 0},
 		velocity = {0, 0, 0},
+		acceleration = {0, 0, 0},
 		damping = 0.95,
 		mass = 1,
 		elasticity = 1.0,
@@ -86,5 +114,45 @@ local blueCircle = sprite:new {
 		radius = 50,
 	},
 }
+
+events.createKeyListener(
+	function(key, bKeyDown)
+		accelerationValue = 100
+		if bKeyDown then
+			if key == "left" then
+				redCircle:setAcceleration(-accelerationValue, 0)
+			elseif key == "right" then
+				redCircle:setAcceleration(accelerationValue, 0)
+			elseif key == "space" then
+				redCircle:setPosition(200, 200)
+			end
+		else
+			redCircle:setAcceleration(0, 0)
+		end		
+end)
+
+events.createCollisionListener(
+	"red.circle",
+	function(circle, _, args)
+		label_penetration:setText("Penetration: " .. args.penetration)
+		label_normal:setText("Contact Normal: " .. tostring(args.contactNormal))
+		label_time_collision:setText("Time Collision: " .. kf.getTick())
+	end,
+	function(circle, _, args)
+		
+end)
+
+timers.createEventInterval(
+	100,
+	function()
+		--label_time_collision:setText("Time: ", kf.getTick())
+end)
+
+kf.addSystem(
+	"Time Label Update",
+	function() end,
+	function()
+		--label_time:setText("Time: ", kf.getTick())
+end)
 
 kf.runMainLoop()
